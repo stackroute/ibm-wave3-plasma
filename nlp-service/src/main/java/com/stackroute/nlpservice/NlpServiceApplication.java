74b5daee27b6465006c2df0e1db6da47be129653
sunigu11 <sunilgupta0739@gmail.com>
2019-02-13 11:54:56 +0530

    nlp service added

---
+++ nlp-service/src/main/java/com/stackroute/nlpservice/NlpServiceApplication.java
@@ -0,0 +1,14 @@
+package com.stackroute.nlpservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NlpServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NlpServiceApplication.class, args);
+	}
+
+}
+
