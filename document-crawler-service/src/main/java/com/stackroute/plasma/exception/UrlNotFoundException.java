b0790a80079cc5a69e58a0f351220442f27e78c7
razeej21 <razeej21@in.ibm.com>
2019-02-16 16:17:26 +0530

    modified

--- document-crawler-service/src/main/java/com/stackroute/plasma/exception/UrlNotFoundException.java
+++ document-crawler-service/src/main/java/com/stackroute/plasma/exception/UrlNotFoundException.java
@@ -1,21 +1,4 @@
-<<<<<<< HEAD
-package com.stackroute.plasma.exception;
 
-public class UrlNotFoundException extends Exception {
-
-
-    private String message;
-    public UrlNotFoundException()
-    {
-
-    }
-    public UrlNotFoundException(String message)
-    {
-        super(message);
-        this.message=message;
-    }
-}
-=======
 //package com.stackroute.plasma.exception;
 //
 //public class UrlNotFoundException extends Exception {
@@ -32,4 +15,4 @@ public class UrlNotFoundException extends Exception {
 //        this.message=message;
 //    }
 //}
->>>>>>> 6a6f0fa3bc436e2a520911cb8bc47272f593cba8
+
