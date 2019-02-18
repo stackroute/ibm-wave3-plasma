101c713f07032578da5af0a82e3e3acea477ea58
dhirajpurbey <dhirajpurbey@gmail.com>
2019-02-18 14:37:11 +0530

    admin working

--- Angular-frontend/src/app/app-routing.module.ts
+++ Angular-frontend/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { WebSpeechComponent } from './plasma/components/web-speech/web-speech.co
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserRegisterComponent } from './plasma/user-register/user-register.component';
+import { DomainExpertComponent } from './plasma/components/domain-expert/domain-expert.component';
 
 
 const routes: Routes = [
@@ -17,6 +18,10 @@ const routes: Routes = [
   {
     path: 'signup',
     component: UserRegisterComponent
+  },
+  {
+    path: 'admin',
+    component: DomainExpertComponent
   }
 ];
 @NgModule({
