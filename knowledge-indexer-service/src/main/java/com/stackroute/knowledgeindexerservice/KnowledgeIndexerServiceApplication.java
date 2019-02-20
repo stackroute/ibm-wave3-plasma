426ff42be6175b4acdcf65090db05c495d152304
jasbatr1 <jaspalkaurbatra95@gmail.com>
2019-02-20 17:40:46 +0530

    Initial Commit of knowledge-indexer-service and removed unwanted code from .properties file

---
+++ knowledge-indexer-service/src/main/java/com/stackroute/knowledgeindexerservice/KnowledgeIndexerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.stackroute.knowledgeindexerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeIndexerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowledgeIndexerServiceApplication.class, args);
+	}
+
+}
