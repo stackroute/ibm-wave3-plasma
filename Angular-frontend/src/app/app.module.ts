95411417111131c9be352ac92b815c68ef4d7fa5
dhirajpurbey <dhirajpurbey@gmail.com>
2019-02-21 11:09:50 +0530

    modified

--- Angular-frontend/src/app/app.module.ts
+++ Angular-frontend/src/app/app.module.ts
@@ -1,6 +1,3 @@
-import { WebSpeechComponent } from './plasma/components/web-speech/web-speech.component';
-
-//  import { WebSpeechModule } from './plasma/components/web-speech/web-speech.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
--- Angular-frontend/src/app/plasma/plasma.module.ts
+++ Angular-frontend/src/app/plasma/plasma.module.ts
@@ -1,11 +1,36 @@
+import { HeaderComponent } from './components/header/header.component';
+import { WebSpeechComponent } from '../plasma/components/web-speech/web-speech.component';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { WebSpeechComponent } from './components/web-speech/web-speech.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from '../app-routing.module';
+
+import { AppComponent } from '../app.component';
+
+
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [WebSpeechComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+
+  ],
   imports: [
-    CommonModule
-  ]
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    WebSpeechComponent,
+    RouterModule,
+  ],
+  exports: [HeaderComponent]
+  // bootstrap: [
+  //   AppComponent
+  // ]
 })
 export class PlasmaModule { }
