416f289e13b5212a6b0da199ec77155ba2866dfa
dhirajpurbey <dhirajpurbey@gmail.com>
2019-02-22 10:28:45 +0530

    modified

--- Angular-frontend/src/app/plasma/plasma.module.ts
+++ Angular-frontend/src/app/plasma/plasma.module.ts
@@ -14,8 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 @NgModule({
   declarations: [
-    HeaderComponent,
-    WebSpeechComponent
+    HeaderComponent
   ],
   imports: [
 
