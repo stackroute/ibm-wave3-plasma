76cf01e2876c95c45e8faeee060027b02ddc6b35
jasbatr1 <jaspalkaurbatra95@gmail.com>
2019-02-26 01:02:39 +0530

    cache-manager-service neo4j implemented

--- cache-manager-service/src/main/java/com/stackroute/cachemanagerservice/repository/IntentRepository.java
+++ cache-manager-service/src/main/java/com/stackroute/cachemanagerservice/repository/IntentRepository.java
@@ -3,6 +3,7 @@ package com.stackroute.cachemanagerservice.repository;
 import com.stackroute.cachemanagerservice.domain.Intent;
 import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.List;
 @Repository
 public interface IntentRepository extends Neo4jRepository<Intent,Long> {
 
-    @Query("MATCH (Intent) RETURN Intent")
-    List<Intent> getAllNodes();
+    @Query("MATCH (n:Intent) where n.type={intent} RETURN n")
+    List<Intent> intent(@Param("intent") String intent);
+
 }
