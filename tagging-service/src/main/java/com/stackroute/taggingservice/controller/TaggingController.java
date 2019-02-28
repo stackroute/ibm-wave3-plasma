2351f1840d2fd9f68e9b742335e8bd6616b7d23a
sunigu11 <sunilgupta0739@gmail.com>
2019-02-28 15:18:45 +0530

    added listner

--- tagging-service/src/main/java/com/stackroute/taggingservice/controller/TaggingController.java
+++ tagging-service/src/main/java/com/stackroute/taggingservice/controller/TaggingController.java
@@ -1,14 +1,12 @@
 package com.stackroute.taggingservice.controller;
 
 import com.stackroute.taggingservice.domain.TagInput;
+import com.stackroute.taggingservice.service.RabbitMQListener;
 import com.stackroute.taggingservice.service.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,16 +18,25 @@ public class TaggingController {
 
     @Autowired
     TagService tagService;
+
+    @Autowired
+    RabbitMQListener rabbitMQListener;
     //List<>
     List<String> temp;
+
 
     public TaggingController() {
     }
 
-    @PostMapping("/tag")
-    public ResponseEntity<?> tagger(@RequestBody  String nlpOutput) {
-        return new ResponseEntity<>(tagService.tagger(nlpOutput), HttpStatus.CREATED);
-       // return new ResponseEntity<>(temp.stream().map(String::toString).collect(Collectors.toList()), HttpStatus.CREATED);
-    }
+//    @PostMapping("/tag")
+//    public ResponseEntity<?> tagger(@RequestBody  String nlpOutput) {
+//        return new ResponseEntity<>(tagService.tagger(tagInput.getTokenizedQuery().toString()), HttpStatus.CREATED);
+//       // return new ResponseEntity<>(temp.stream().map(String::toString).collect(Collectors.toList()), HttpStatus.CREATED);
+//    }
 
+
+    @GetMapping("/tag")
+    public  ResponseEntity<?> tagger() {
+        return new ResponseEntity<>(tagService.tagger(rabbitMQListener.getTagInputt().toString()),HttpStatus.OK);
+    }
 }
--- tagging-service/src/main/java/com/stackroute/taggingservice/service/RabbitMQListener.java
+++ tagging-service/src/main/java/com/stackroute/taggingservice/service/RabbitMQListener.java
@@ -8,11 +8,28 @@ import org.springframework.stereotype.Service;
 @Service
 public class RabbitMQListener {
 
-    //TagInput tagInput = new TagInput();
+    TagInput tagInputt;
+
+    public TagInput getTagInputt() {
+        return tagInputt;
+    }
+
+    public void setTagInputt(TagInput tagInputt) {
+        this.tagInputt = tagInputt;
+    }
+
+    public RabbitMQListener(TagInput tagInputt) {
+        this.tagInputt = tagInputt;
+    }
 
     @RabbitListener(queues = "${javainuse4.rabbitmq.queue}", containerFactory = "jsaFactory")
     public void consume(TagInput tagInput){
+        //tagInputt.setTokenizedQuery(tagInput);
         System.out.println("Recieved Message From RabbitMQ: " + tagInput.toString());
+        //this.tagInputt=tagInput;
+        //TagInput tagInput1 = new TagInput(tagInput.getTokenizedQuery());
+        RabbitMQListener rabbitMQListener = new RabbitMQListener(tagInput);
+        //this.tagInputt.setTokenizedQuery(tagInput.getTokenizedQuery());
     }
 
 }
