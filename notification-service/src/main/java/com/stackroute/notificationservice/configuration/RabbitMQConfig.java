02d9b616c5a1635a5396f89596280679b930938a
jasbatr1 <jaspalkaurbatra95@gmail.com>
2019-03-04 14:27:32 +0530

    RMQ implemented in notification-service

--- notification-service/src/main/java/com/stackroute/notificationservice/configuration/RabbitMQConfig.java
+++ notification-service/src/main/java/com/stackroute/notificationservice/configuration/RabbitMQConfig.java
@@ -1,28 +1,30 @@
 package com.stackroute.notificationservice.configuration;
-import org.springframework.amqp.core.*;
+
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.amqp.SimpleRabbitListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 @Configuration
 public class RabbitMQConfig {
-    @Bean
-    public MessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
 
-    }
+    //Receiving message rabbitMQ
     @Bean
-    public SimpleRabbitListenerContainerFactory jsaFactory(ConnectionFactory connectionFactory,SimpleRabbitListenerContainerFactoryConfigurer configurer) {
+    public MessageConverter consumerJsonMessageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory jsaFactory(ConnectionFactory connectionFactory,
+                                                           SimpleRabbitListenerContainerFactoryConfigurer configurer) {
         SimpleRabbitListenerContainerFactory factory =
                 new SimpleRabbitListenerContainerFactory();
         configurer.configure(factory, connectionFactory);
-        factory.setMessageConverter(jsonMessageConverter());
+        factory.setMessageConverter(consumerJsonMessageConverter());
         return factory;
     }
-}
 
+}
