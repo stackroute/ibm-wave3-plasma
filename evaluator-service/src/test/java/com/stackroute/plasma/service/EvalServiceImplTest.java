3ec9185a6f89b2163f929fc20f3e3ac7c7550ed0
chandub15 <chandraprakashb15@gmail.com>
2019-03-06 17:13:38 +0530

    algorithm updated

--- evaluator-service/src/test/java/com/stackroute/plasma/service/EvalServiceImplTest.java
+++ evaluator-service/src/test/java/com/stackroute/plasma/service/EvalServiceImplTest.java
@@ -21,7 +21,7 @@ public class EvalServiceImplTest {
     Evaluator evaluator;
 
     private Url url;
-    private EvaluatorServiceImpl evalServiceImpl;
+    private EvaluatorService evalServiceImpl;
     private Timestamp timestamp;
     private String doc;
     DecimalFormat df2 = new DecimalFormat("#.##");
@@ -31,7 +31,6 @@ public class EvalServiceImplTest {
 
     @Before
     public void setUp() throws Exception {
-        evalServiceImpl=new EvaluatorServiceImpl();
         evaluator=new Evaluator();
         url=new Url();
         url.setDomain("java");
@@ -55,7 +54,6 @@ public class EvalServiceImplTest {
     @After
     public void tearDown() throws Exception {
         evaluator=null;
-        evalServiceImpl=null;
     }
 
     @Test
