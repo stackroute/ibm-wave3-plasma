df31077e7f143e7cbb749ee096a91115c621ecf2
razeej21 <razeej21@in.ibm.com>
2019-03-11 18:56:23 +0530

    Making page responsive

--- plasma-frontend/src/app/plasma/components/card/card.component.ts
+++ plasma-frontend/src/app/plasma/components/card/card.component.ts
@@ -1,5 +1,6 @@
 import { CardService } from './../../services/card.service';
 import { Component, OnInit } from '@angular/core';
+import { WebSocketService } from '../../services/web-socket.service';
 
 @Component({
   selector: 'app-card',
@@ -8,12 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
    private resp: any ;
-  constructor(private Cardservice: CardService ) { }
-
+  constructor( private ws:  WebSocketService) { }
+  // public resp;
   ngOnInit() {
-    this.Cardservice.getdoc().subscribe(data => {
-      this.resp = data ;
-        console.log('response from getQuiz in card', data);
+    // this.Cardservice.getdoc().subscribe(data => {
+    //   this.resp = data ;
+    //     console.log('response from getQuiz in card', data);
+    // });
+    console.log('this is cards component !!');
+    this.ws.dataFromTopic.subscribe(({body}) => {
+        console.log(JSON.parse(body), 'this is the data ');
+        this.resp = JSON.parse(body);
     });
   }
 
