dbba3911c1d82bc711a5a44a1252358a6557e2b8
razeej21 <razeej21@in.ibm.com>
2019-03-15 09:55:43 +0530

    modified

--- plasma-frontend/src/app/plasma/components/user-login/user-login.component.ts
+++ plasma-frontend/src/app/plasma/components/user-login/user-login.component.ts
@@ -34,7 +34,8 @@ private password: string;
      console.log(this.userAuth);
     console.log(this.value);
     this.loginService.login(this.userAuth).subscribe((res: any) => {
-      window.alert('User logged in successfully');
+      // window.alert('User logged in successfully');
+      this.snackBar.open('User does not exist !!!', '', { duration: 2000, verticalPosition: 'bottom' });
       console.log('Res: ', res);
       console.log(this.helper.decodeToken(res.body.token).jti, ' :this is the value of the userId');
     console.log(this.helper.decodeToken(res.body.token).sub, ' :this is the value of the role');
