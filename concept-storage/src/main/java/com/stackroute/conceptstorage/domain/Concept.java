d081d1f64f9d8444124685343dc023345b01db37
Nalinshankar <nalinmakaut@gmail.com>
2019-03-15 10:01:24 +0530

    snack-bar modified

--- concept-storage/src/main/java/com/stackroute/conceptstorage/domain/Concept.java
+++ concept-storage/src/main/java/com/stackroute/conceptstorage/domain/Concept.java
@@ -1,4 +1,34 @@
 package com.stackroute.conceptstorage.domain;
 
+import org.apache.catalina.LifecycleState;
+
+import java.util.List;
+
 public class Concept {
+    private String domain;
+    private List<String> concept;
+
+    public Concept(String domain, List<String> concept) {
+        this.domain = domain;
+        this.concept = concept;
+    }
+
+    public Concept() {
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+    public List<String> getConcept() {
+        return concept;
+    }
+
+    public void setConcept(List<String> concept) {
+        this.concept = concept;
+    }
 }
