45586a72204c2b54e0506e94f6de4d2e87a55cd2
sunigu11 <sunilgupta0739@gmail.com>
2019-03-17 14:18:38 +0530

    tagging-service algorithm updated

--- tagging-service/src/main/java/com/stackroute/taggingservice/domain/TagOutput.java
+++ tagging-service/src/main/java/com/stackroute/taggingservice/domain/TagOutput.java
@@ -2,11 +2,13 @@ package com.stackroute.taggingservice.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class TagOutput {
 
     private List<String> taggedConcept;
